eb1a2954e1331eb4823a99b2fefa510d086b067d
JAGdeRoos <47323566+JAGdeRoos@users.noreply.github.com>
2023-12-02 00:51:19 +0100

    Create main.cpp

---
+++ 2023/day01/trebuchet/main.cpp
@@ -0,0 +1,68 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QStringList>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QFile* file = new QFile("../input.txt");
+
+    if (!file->open(QIODevice::ReadOnly | QIODevice::Text))
+        return 0;
+
+    int iFirst = 0;
+    int iSecond= 0;
+    int iNumber= 0;
+    int iTotal = 0;
+    int iFirstNumberIndex = 0;
+    int iSecondNumberIndex = 0;
+
+    QStringList numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    QString line;
+
+    while (!file->atEnd())
+    {
+        line = file->readLine();
+
+        iFirstNumberIndex = line.size();
+        iSecondNumberIndex = -1;
+        for (const auto &number : numbers){
+            if (line.contains(number))
+            {
+                if(line.indexOf(number) < iFirstNumberIndex)
+                {
+                    iFirst = &number - &numbers[0];
+                    iFirstNumberIndex = line.indexOf(number);
+                }
+                if(line.lastIndexOf(number) > iSecondNumberIndex)
+                {
+                    iSecond = &number - &numbers[0];
+                    iSecondNumberIndex = line.lastIndexOf(number);
+                }
+            }
+        }
+        for (int i = 0; i < line.size(); i++){
+            if(line.at(i).isDigit())
+            {
+                if(i < iFirstNumberIndex)
+                    iFirst = line.at(i).digitValue();
+                break;
+            }
+        }
+        for (int i = line.size()-1; i >= 0; i--){
+            if(line.at(i).isDigit())
+            {
+                if(i > iSecondNumberIndex)
+                    iSecond = line.at(i).digitValue();
+                break;
+            }
+        }
+
+        iNumber = QString(QString::number(iFirst) + QString::number(iSecond)).toInt();
+        iTotal += iNumber;
+        qDebug() << iNumber << iTotal << line;
+    }
+    return a.exec();
+}
