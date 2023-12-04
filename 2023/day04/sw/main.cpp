ba9423f97aebb048c74a0d069cf9ed9c71d2b9ce
JAGdeRoos <47323566+JAGdeRoos@users.noreply.github.com>
2023-12-04 22:18:09 +0100

    Create main.cpp

---
+++ 2023/day04/sw/main.cpp
@@ -0,0 +1,84 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QStringList>
+
+struct strNumber{
+    int value;
+    int lineNr;
+    QVector<int> index;
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QFile* file = new QFile("../input.txt");
+
+    if (!file->open(QIODevice::ReadOnly | QIODevice::Text))
+        return 0;
+
+    QString line = "";
+    QString winningNumbersString;
+    QString numbersYouHaveString;
+    QStringList winningNumbers;
+    QStringList numbersYouHave;
+
+    int iIndex=0;
+    int iID=0;
+
+    int iCurrentCardPoints;
+    int iNumberOfMatches;
+    int iFirstTotalPoints = 0;
+    int iSecondTotalPoints = 0;
+
+    bool bFirstMatch;
+
+    QVector<int> copies(198,1);
+
+    qDebug() << copies;
+    copies.append(0);
+
+    while(!file->atEnd())
+    {
+        line = file->readLine();
+
+        line.remove("Card ");
+        iIndex = line.lastIndexOf(":");
+        iID = line.left(iIndex).toInt();
+
+        line = line.remove(0,line.indexOf(":")+1);
+        line = line.remove("\n");
+
+        winningNumbersString = line.split("|").at(0);
+
+        winningNumbers = line.split("|").at(0).split(" ", Qt::SkipEmptyParts);
+        numbersYouHave = line.split("|").at(1).split(" ", Qt::SkipEmptyParts);
+
+        iNumberOfMatches = 0;
+        iCurrentCardPoints = 0;
+        bFirstMatch = true;
+        for(const QString &number : numbersYouHave)
+            for(const QString &winning : winningNumbers)
+                if (number == winning)
+                {
+                    if (bFirstMatch)
+                    {
+                        bFirstMatch = false;
+                        iCurrentCardPoints = 1;
+                    }
+                    else
+                    {
+                        iCurrentCardPoints *= 2;
+                    }
+                    iNumberOfMatches++;
+                }
+        iFirstTotalPoints += iCurrentCardPoints;
+
+        for(int i=0; i<iNumberOfMatches; i++)
+            copies[iID+i] += copies[iID-1];
+
+        iSecondTotalPoints += copies[iID-1];
+        qDebug() << iID << iFirstTotalPoints << iSecondTotalPoints << copies[iID-1] << iNumberOfMatches;
+    }
+    return a.exec();
+}
