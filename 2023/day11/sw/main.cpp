4ebc87f1c1b30ddd35547ccd28a661a9aeef4654
JAGdeRoos <deroosjochem@gmail.com>
2023-12-12 16:55:06 +0100

    completed day 11

--- 2023/day11/sw/main.cpp
+++ 2023/day11/sw/main.cpp
@@ -3,6 +3,14 @@
 #include <QStringList>
 #include <QTime>
 #include <QElapsedTimer>
+#include <QPoint>
+
+#define AMOUNT 1000000
+
+struct Galaxy{
+    QPoint location;
+    long long number;
+};
 
 int main(void)
 {
@@ -17,9 +25,8 @@ int main(void)
         return 1;
     }
     QString line = "";
-    QStringList lineList;
 
-    QVector<QVector<long long>> numbers;
+    QVector<QVector<QString>> space;
 
     // read file
     while(!file.atEnd())
@@ -28,25 +35,102 @@ int main(void)
         line = line.remove("\n");
         if (!line.isEmpty())
         {
-            lineList = line.split(" ", Qt::SkipEmptyParts);
+            space.append(line.split("", Qt::SkipEmptyParts));
         }
-        QVector<long long> lineNumbers;
-        for(const auto &element : lineList)
-            lineNumbers.append(element.toLongLong());
-        numbers.append(lineNumbers);
     }
 
     long long total1=0;
     long long total2=0;
     // solve puzzle...
+\
 
+    QVector<Galaxy> galaxies;
+    QVector<bool> rowEmpty;
+    QVector<bool> colEmpty;
+    rowEmpty.resize(space.size(), true);
+    colEmpty.resize(space.size(), true);
 
+    // find empty rows/columns
+    long long iter = 0;
+    for(long long y = 0; y < space.size(); y++)
+    {
+        for(long long x = 0; x < space.at(y).size(); x ++)
+        {
+            if(space.at(y).at(x) == "#")
+            {
+                Galaxy found;
+                found.number = ++iter;
+                found.location = QPoint(x,y);
+                galaxies.append(found);
 
+                rowEmpty[y] = false;
+                colEmpty[x] = false;
+            }
+        }
+    }
 
+    for(long long i = 0; i < galaxies.size(); i++)
+    {
+        for(long long j = i; j < galaxies.size(); j++)
+        {
+            if(i == j)
+                continue;
+            long long xDiff1 = 0;
+            long long yDiff1 = 0;
 
+            long long xDiff2 = 0;
+            long long yDiff2 = 0;
 
+            long long nrOfCols1 = 0;
+            long long nrOfRows1 = 0;
 
+            long long nrOfCols2 = 0;
+            long long nrOfRows2 = 0;
 
+            if(galaxies.at(i).location.x() != galaxies.at(j).location.x())
+            {
+                for(int x = qMin(galaxies.at(i).location.x(), galaxies.at(j).location.x());
+                        x < qMax(galaxies.at(i).location.x(),galaxies.at(j).location.x());
+                        x++)
+                {
+                    if(colEmpty.at(x))
+                    {
+                        nrOfRows1 += 2;
+                        nrOfRows2 += AMOUNT;
+                    }
+                    else
+                    {
+                        nrOfRows1 ++;
+                        nrOfRows2 ++;
+                    }
+                }
+                xDiff1 = nrOfRows1;
+                xDiff2 = nrOfRows2;
+            }
+            if(galaxies.at(i).location.y() != galaxies.at(j).location.y())
+            {
+                for(int y = qMin(galaxies.at(i).location.y(),galaxies.at(j).location.y());
+                        y < qMax(galaxies.at(i).location.y(),galaxies.at(j).location.y());
+                        y++)
+                {
+                    if(rowEmpty.at(y))
+                    {
+                        nrOfCols1 += 2;
+                        nrOfCols2 += AMOUNT;
+                    }
+                    else
+                    {
+                        nrOfCols1 ++;
+                        nrOfCols2 ++;
+                    }
+                }
+                yDiff1 = nrOfCols1;
+                yDiff2 = nrOfCols2;
+            }
+            total1 += xDiff1 + yDiff1;
+            total2 += xDiff2 + yDiff2;
+        }
+    }
 
     QTime elapsed(0, 0);
     elapsed = elapsed.addMSecs(timer.elapsed());
